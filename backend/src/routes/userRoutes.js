59cb66be529114fb80681ad336ce8ece16d13caf
uimarshall <uimarshall@gmail.com>
2023-07-14 00:20:38 +0100

    Add user registration and login
    In this PR, the user registration and login are added. The user registration is done by the user registration controller and the user login is done by the user login controller. The user registration and login are added to the user routes. The user registration and login are tested with Postman.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import registerUser from '../controllers/userAuthControllers.js';
+import { registerUser, loginUser } from '../controllers/userAuthControllers.js';
 
 const router = Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
