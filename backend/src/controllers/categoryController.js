b5ec8aa99233199ac99ee7fab4c99b2fe89eb2b6
uimarshall <uimarshall@gmail.com>
2023-07-17 16:28:18 +0100

    Add functionality to get single and all categories In this commit, I add functionality to get single and all categories. I also add a route to get all categories and a route to get a single category. I also add a controller to handle the get all categories route and the get single category route.

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -9,11 +9,14 @@ import ErrorHandler from '../utils/errorHandler.js';
 // @access: private
 const createCategory = asyncHandler(async (req, res, next) => {
   const { name } = req.body;
+
   const slug = slugify(name).toLowerCase();
   const categoryExists = await Category.findOne({ name });
   if (categoryExists) {
     return next(new ErrorHandler('Category already exists', StatusCodes.BAD_REQUEST));
   }
+
+  // name.charAt(0).toUpperCase();
 
   const newCategory = await Category.create({ name, slug });
   return res.status(StatusCodes.CREATED).json({
@@ -23,4 +26,35 @@ const createCategory = asyncHandler(async (req, res, next) => {
   });
 });
 
-export default createCategory;
+// @desc: Get all categories
+// @route: /api/v1/category/list-all
+// @access: public
+const getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find({});
+  console.log(categories);
+  return res.status(StatusCodes.OK).json({
+    success: true,
+    message: 'All categories',
+    data: categories,
+  });
+});
+
+// @desc: Get a single category
+// @route: /api/v1/category/:slug
+// @access: public
+const getSingleCategory = asyncHandler(async (req, res, next) => {
+  const { slug } = req.params;
+  const categoryFound = await Category.findOne({ slug });
+  if (!categoryFound) {
+    return next(new ErrorHandler('Category not found', StatusCodes.NOT_FOUND));
+  }
+
+  // TODO: Add the associated blogs to this category
+  return res.status(StatusCodes.OK).json({
+    success: true,
+    message: 'Category found',
+    data: categoryFound,
+  });
+});
+
+export { createCategory, getAllCategories, getSingleCategory };
