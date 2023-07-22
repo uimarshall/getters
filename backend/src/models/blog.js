f9eaa78c22ec98b6a7f3b18652c1df755ef2b63e
uimarshall <uimarshall@gmail.com>
2023-07-22 22:56:39 +0100

    Fix bug in the BlogPostController. In this PR, I fixed a bug in the BlogPostController. The bug was that the BlogPostController was not trimming the blog post before saving it to the database and could not populate the categories and tags field. This commit fixes that bug.

--- backend/src/models/blog.js
+++ backend/src/models/blog.js
@@ -1,6 +1,6 @@
 import mongoose, { model, Schema } from 'mongoose';
 
-const { ObjectId } = mongoose.Schema;
+const { ObjectId } = mongoose.Schema.Types;
 
 const blogSchema = new Schema(
   {
@@ -16,36 +16,62 @@ const blogSchema = new Schema(
       unique: true,
       index: true,
     },
-    // body: {
-    //   type: {},
-    //   required: [true, 'Please enter blog body'],
-    //   minlength: [20, 'Blog body must be at least 200 characters long'],
-    //   maxlength: [2000000, 'Blog body cannot exceed 2,000,000 characters'],
-    // },
-    // excerpt: {
-    //   type: String,
-    //   maxlength: [1000, 'Blog excerpt cannot exceed 1000 characters'],
-    // },
+    body: {
+      type: {},
+      required: [true, 'Please enter blog body'],
+      minlength: [20, 'Blog body must be at least 200 characters long'],
+      maxlength: [2000000, 'Blog body cannot exceed 2,000,000 characters'],
+    },
+    excerpt: {
+      type: String,
+      maxlength: [1000, 'Blog excerpt cannot exceed 1000 characters'],
+    },
     metaTitle: {
       type: String,
     },
-    // metaDesc: {
-    //   type: String,
+    metaDesc: {
+      type: String,
+    },
+    // photo: {
+    //   data: Buffer,
+    //   contentType: String,
     // },
     photo: {
-      data: Buffer,
-      contentType: String,
+      type: String,
+      default: 'https://via.placeholder.com/728x400.png?text=Visit+WhoAmI',
+    },
+    claps: {
+      type: Number,
+      default: 0,
     },
     categories: [{ type: ObjectId, ref: 'Category', required: [true, 'Post category is required'] }],
     tags: [{ type: ObjectId, ref: 'Tag', required: true }],
-    postedBy: {
+    author: {
       type: ObjectId,
       ref: 'User',
       required: [true, 'Please author of post is required'],
     },
+    shares: {
+      type: Number,
+      default: 0,
+    },
+    postViews: {
+      type: Number,
+      default: 0,
+    },
+    schedulePublications: {
+      type: Date,
+      default: null,
+    },
     numberOfViews: [{ type: ObjectId, ref: 'User' }],
     likes: [{ type: ObjectId, ref: 'User' }],
     disLikes: [{ type: ObjectId, ref: 'User' }],
+    comments: [
+      {
+        type: ObjectId,
+        ref: 'Comment',
+      },
+    ],
   },
   { timestamps: true }
 );
