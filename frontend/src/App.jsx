2c8cbf181023b61f04151cffe9ccddecac6b6f3c
uimarshall <uimarshall@gmail.com>
2023-09-03 19:49:35 +0100

    Create react app for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <p>Gethub</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
